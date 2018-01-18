2d47def4b9d4de7d19186b20059c3137d67bd30c
Joerg Spoerl <joerg.spoerl@gmx.de>
2018-01-18 21:36:52 +0100

    now use MediaControll

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,10 +9,11 @@ import { ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  @ViewChild('myplayer1') playerElement;
+  @ViewChild('myplayer1') playerElement: ElementRef;
   @ViewChild('test') testElement: ElementRef;
 
   private audioContext: AudioContext;
+  private MediaPlayer: MediaElementAudioSourceNode;
   private loadingSample: boolean = false;
   private audioBuffer: AudioBuffer;
   private bufferSource: AudioBufferSourceNode;
@@ -26,6 +27,18 @@ export class HomePage implements OnInit {
   ngOnInit() {
     this.audioContext = new AudioContext();
 
+    this.MediaPlayer  = this.audioContext.createMediaElementSource(this.playerElement.nativeElement);
+    this.playerElement.nativeElement.addEventListener("play", 
+      () => { 
+        console.log("play"); 
+        this.initEq(this.audioContext, this.MediaPlayer)
+      }, true);
+
+    this.initSourceBuffer();
+
+  }
+
+  initSourceBuffer() {
     this.loadingSample = true;
     this.fetchSample()
         .then(audioBuffer => {
@@ -54,7 +67,7 @@ export class HomePage implements OnInit {
     this.bufferSource = this.audioContext.createBufferSource();
     this.bufferSource.buffer = this.audioBuffer;
     this.bufferSource.connect(this.audioContext.destination);
-    this.initEq();
+    this.initEq(this.audioContext, this.bufferSource);
     this.bufferSource.start(0);
 }
 
@@ -66,33 +79,33 @@ export class HomePage implements OnInit {
     this.bufferSource.stop(0);
   }
 
-  initEq () {
+  initEq (audioContext, source) {
       //   // EQ Properties
   //   //
     var gainDb = -40.0;
     var bandSplit = [360, 3600];
 
-    var hBand = this.audioContext.createBiquadFilter();
+    var hBand = audioContext.createBiquadFilter();
     hBand.type = "lowshelf";
     hBand.frequency.value = bandSplit[0];
     hBand.gain.value = gainDb;
 
-    var hInvert = this.audioContext.createGain();
+    var hInvert = audioContext.createGain();
     hInvert.gain.value = -1.0;
 
-    var mBand = this.audioContext.createGain();
+    var mBand = audioContext.createGain();
 
-    var lBand = this.audioContext.createBiquadFilter();
+    var lBand = audioContext.createBiquadFilter();
     lBand.type = "highshelf";
     lBand.frequency.value = bandSplit[1];
     lBand.gain.value = gainDb;
 
-    var lInvert = this.audioContext.createGain();
+    var lInvert = audioContext.createGain();
     lInvert.gain.value = -1.0;
 
-    this.bufferSource.connect(lBand);
-    this.bufferSource.connect(mBand);
-    this.bufferSource.connect(hBand);
+    source.connect(lBand);
+    source.connect(mBand);
+    source.connect(hBand);
 
     hBand.connect(hInvert);
     lBand.connect(lInvert);
@@ -100,21 +113,21 @@ export class HomePage implements OnInit {
     hInvert.connect(mBand);
     lInvert.connect(mBand);
 
-    this.lGain = this.audioContext.createGain();
-    this.mGain = this.audioContext.createGain();
-    this.hGain = this.audioContext.createGain();
+    this.lGain = audioContext.createGain();
+    this.mGain = audioContext.createGain();
+    this.hGain = audioContext.createGain();
 
     lBand.connect(this.lGain);
     mBand.connect(this.mGain);
     hBand.connect(this.hGain);
 
-    var sum = this.audioContext.createGain();
+    var sum = audioContext.createGain();
     this.lGain.connect(sum);
     this.mGain.connect(sum);
     this.hGain.connect(sum);
-    sum.connect(this.audioContext.destination);
+    sum.connect(audioContext.destination);
 
-    this.lGain.gain.value = 0
+    //this.lGain.gain.value = 0
   }
 
 
